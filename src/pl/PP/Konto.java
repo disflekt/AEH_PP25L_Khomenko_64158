ac73e39a1ece55d2da11f77433ecd83de4c1869a
disflekt <mkhomenko229@gmail.com>
2025-06-02 12:28:36 +0200

    .

---
+++ src/pl/PP/Konto.java
@@ -0,0 +1,109 @@
+package pl.PP;
+
+public class Konto {
+
+    private String numerKonta;
+    private String imieWlasciciela;
+    private String nazwiskoWlasciciela;
+    private String emailWlasciciela;
+    private String telefonWlasciciela;
+    private double kwotaPieniedzy;
+
+
+    public Konto() {
+        this.kwotaPieniedzy = 1000.0; // Początkowa kwota PLN 1000
+    }
+
+
+    public Konto(String numerKonta, String imieWlasciciela, String nazwiskoWlasciciela,
+                 String emailWlasciciela, String telefonWlasciciela, double kwotaPieniedzy) {
+        this.numerKonta = numerKonta;
+        this.imieWlasciciela = imieWlasciciela;
+        this.nazwiskoWlasciciela = nazwiskoWlasciciela;
+        this.emailWlasciciela = emailWlasciciela;
+        this.telefonWlasciciela = telefonWlasciciela;
+        this.kwotaPieniedzy = kwotaPieniedzy;
+    }
+
+
+    public String getNumerKonta() {
+        return numerKonta;
+    }
+
+    public void setNumerKonta(String numerKonta) {
+        this.numerKonta = numerKonta;
+    }
+
+    public String getImieWlasciciela() {
+        return imieWlasciciela;
+    }
+
+    public void setImieWlasciciela(String imieWlasciciela) {
+        this.imieWlasciciela = imieWlasciciela;
+    }
+
+    public String getNazwiskoWlasciciela() {
+        return nazwiskoWlasciciela;
+    }
+
+    public void setNazwiskoWlasciciela(String nazwiskoWlasciciela) {
+        this.nazwiskoWlasciciela = nazwiskoWlasciciela;
+    }
+
+    public String getEmailWlasciciela() {
+        return emailWlasciciela;
+    }
+
+    public void setEmailWlasciciela(String emailWlasciciela) {
+        this.emailWlasciciela = emailWlasciciela;
+    }
+
+    public String getTelefonWlasciciela() {
+        return telefonWlasciciela;
+    }
+
+    public void setTelefonWlasciciela(String telefonWlasciciela) {
+        this.telefonWlasciciela = telefonWlasciciela;
+    }
+
+    public double getKwotaPieniedzy() {
+        return kwotaPieniedzy;
+    }
+
+    public void setKwotaPieniedzy(double kwotaPieniedzy) {
+        this.kwotaPieniedzy = kwotaPieniedzy;
+    }
+
+
+    public void wplac(double kwota) {
+        if (kwota > 0) {
+            this.kwotaPieniedzy += kwota;
+            System.out.println("Wpłacono " + kwota + " PLN. Nowe saldo: " + this.kwotaPieniedzy + " PLN");
+        } else {
+            System.out.println("Kwota do wpłaty musi być większa od 0");
+        }
+    }
+
+
+    public void wyplac(double kwota) {
+        if (kwota > 0 && kwota <= this.kwotaPieniedzy) {
+            this.kwotaPieniedzy -= kwota;
+            System.out.println("Wypłacono " + kwota + " PLN. Pozostałe saldo: " + this.kwotaPieniedzy + " PLN");
+        } else if (kwota > this.kwotaPieniedzy) {
+            System.out.println("Brak środków. Maksymalna kwota do wypłaty: " + this.kwotaPieniedzy + " PLN");
+        } else {
+            System.out.println("Kwota do wypłaty musi być większa od 0");
+        }
+    }
+
+
+    public void wyswietlInfo() {
+        System.out.println("=== INFORMACJE O KONCIE ===");
+        System.out.println("Numer konta: " + numerKonta);
+        System.out.println("Właściciel: " + imieWlasciciela + " " + nazwiskoWlasciciela);
+        System.out.println("Email: " + emailWlasciciela);
+        System.out.println("Telefon: " + telefonWlasciciela);
+        System.out.println("Saldo: " + kwotaPieniedzy + " PLN");
+        System.out.println("===========================");
+    }
+}
---
+++ src/pl/PP/mojaOsmaAplikacja.java
@@ -0,0 +1,31 @@
+package pl.PP;
+import pl.PP.Konto ;
+
+public class mojaOsmaAplikacja {
+    public static void main(String[] args) {
+
+        Konto konto1 = new Konto();
+        konto1.setNumerKonta("123456789");
+        konto1.setImieWlasciciela("Jan");
+        konto1.setNazwiskoWlasciciela("Kowalski");
+        konto1.setEmailWlasciciela("jan.kowalski@email.com");
+        konto1.setTelefonWlasciciela("123-456-789");
+
+        konto1.wyswietlInfo();
+
+        konto1.wyplac(900.0);
+        konto1.wyswietlInfo();
+
+        konto1.wplac(250.0);
+        konto1.wyswietlInfo();
+
+        String numerKonta = konto1.getNumerKonta();
+        double saldo = konto1.getKwotaPieniedzy();
+        System.out.println("Pobrane dane: numer konta " + numerKonta + " z saldem = " + saldo + " PLN");
+
+        Konto konto2 = new Konto("987654321", "Anna", "Nowak", "anna.nowak@email.com", "987-654-321", 500.0);
+        konto2.wyswietlInfo();
+        konto2.setImieWlasciciela("ZmienioneImie");
+        konto2.wyswietlInfo();
+    }
+}
