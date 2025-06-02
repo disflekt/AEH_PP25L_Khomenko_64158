a29c29e9fe670d3d19e1263fa37c83639be03d73
disflekt <mkhomenko229@gmail.com>
2025-06-02 11:38:11 +0200

    .

---
+++ pl.PP/lab5/src/pl/PP/mojaPiataAplikacja.java
@@ -0,0 +1,45 @@
+package pl.PP;
+
+public class mojaPiataAplikacja {
+    public static void main(String[] args) {
+
+        // Wywołanie nowej metody z różnymi argumentami
+        System.out.println("=== Demonstracja nowej metody ===");
+
+        // Pierwsze wywołanie
+        displayPattern("*", 5, 3);
+
+        System.out.println(); // Pusta linia dla separacji
+
+        // Drugie wywołanie
+        displayPattern("#", 8, 4);
+
+        System.out.println(); // Pusta linia dla separacji
+
+        // Trzecie wywołanie
+        displayPattern("@", 10, 2);
+    }
+
+    /**
+     * Metoda wyświetlająca wzór na konsoli
+     * @param znak - znak do wyświetlenia
+     * @param liczbaZnakow - liczba znaków w każdej linii (liczba całkowita)
+     * @param liczbLinii - liczba linii do wyświetlenia
+     */
+    private static void displayPattern(String znak, int liczbaZnakow, int liczbLinii) {
+        System.out.println("Wyświetlam wzór ze znakiem: " + znak);
+        System.out.println("Liczba znaków w linii: " + liczbaZnakow);
+        System.out.println("Liczba linii: " + liczbLinii);
+        System.out.println("Wzór:");
+
+        // Pętla dla liczby linii
+        for (int i = 0; i < liczbLinii; i++) {
+            // Pętla dla liczby znaków w każdej linii
+            for (int j = 0; j < liczbaZnakow; j++) {
+                System.out.print(znak);
+            }
+            System.out.println(); // Nowa linia po każdym rzędzie
+        }
+        System.out.println("--- Koniec wzoru ---");
+    }
+}
